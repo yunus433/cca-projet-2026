1b51b7e420f90416f3cb15be607c6912ff40e6fe
yunus433 <yunus.gurlek@etu.sorbonne-universite.fr>
2026-01-24 20:31:39 +0100

    Add euclid GCD algo

---
+++ code/gcd/euclid.c
@@ -0,0 +1,58 @@
+#ifndef EUCLIDEAN_H
+#define EUCLIDEAN_H
+
+#include <flint/flint.h>
+#include <flint/fmpz.h>
+#include <flint/fmpz_poly.h>
+#include <flint/fmpz_mat.h>
+
+int fmpz_poly_gcd_euclid(
+  fmpz_poly_t r,
+  const fmpz_poly_t p,
+  const fmpz_poly_t q
+) {
+  if (fmpz_poly_is_zero(p) && fmpz_poly_is_zero(q))
+    return -1;
+
+  fmpz_poly_t a, b, tmp;
+  slong d;
+  fmpz_poly_init(a);
+  fmpz_poly_init(b);
+  fmpz_poly_init(tmp);
+
+  fmpz_poly_set(a, p);
+  fmpz_poly_set(b, q);
+
+  while (!fmpz_poly_is_zero(b)) {
+    fmpz_poly_pseudo_rem(tmp, &d, a, b);
+    fmpz_poly_set(a, b);
+    fmpz_poly_set(b, tmp);
+  }
+
+  fmpz_poly_set(r, a);
+
+  return 1;
+}
+
+#endif
+
+int main() {
+  fmpz_poly_t f, g, q, r;
+  fmpz_poly_init(f);
+  fmpz_poly_init(g);
+  fmpz_poly_init(q);
+  fmpz_poly_init(r);
+
+  fmpz_poly_set_coeff_si(f, 0, 6);
+  fmpz_poly_set_coeff_si(f, 1, 7);
+  fmpz_poly_set_coeff_si(f, 2, 1);
+
+  fmpz_poly_set_coeff_si(g, 0, -6);
+  fmpz_poly_set_coeff_si(g, 1, -5);
+  fmpz_poly_set_coeff_si(g, 2, 1);
+
+  fmpz_poly_gcd_euclid(r, f, g);
+
+  fmpz_poly_print(r);
+  printf("\n");
+}
