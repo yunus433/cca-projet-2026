9dd4c1b779011b8eddd47e7ea8d99dde2fa78553
yunus433 <yunus.gurlek@etu.sorbonne-universite.fr>
2026-02-24 17:42:16 +0100

    Finish 1st version of tests

--- code/src/euclidean_division/test_euclidean_division.c
+++ code/src/euclidean_division/test_euclidean_division.c
@@ -1,25 +1,77 @@
-#include "euclidean_division.h"
+// gcc-15 -O2 euclidean_division.h test_euclidean_division.c  $(pkg-config --cflags --libs flint)
 
-int _fmpq_poly_euclidean_division_test(
+#include <time.h>
+
+#include "euclidean_division.c"
+
+int COEFF_BIT_SIZE = 5;
+
+int fmpq_poly_euclidean_division_test(
   int degree,
   int count
 ) {
-  flint_rand_t state;
-  fmpq_poly_t A, B;
+  flint_rand_t rand_state;
+  fmpq_poly_t Q, R, S, A, B;
 
-  flint_rand_init(state);
+  flint_rand_init(rand_state);
+  flint_rand_set_seed(rand_state, time(NULL), time(NULL));
+
+  fmpq_poly_init(Q);
+  fmpq_poly_init(R);
+  fmpq_poly_init(S);
+  fmpq_poly_init(A);
+  fmpq_poly_init(B);
+
+  int length = degree + 1;
 
   for (int i = 0; i < count; i++) {
     printf("Starting test %d...\n", i+1);
     
-    fmpq_poly_init(B);
-    fmpq_poly_randtest(B, state, /*len*/ 10, /*bits*/ 30);   // Q[x]
-    fmpq_poly_print(B); flint_printf("\n");
+    fmpq_poly_randtest(A, rand_state, length + 1, COEFF_BIT_SIZE);
+    fmpq_poly_randtest(B, rand_state, length, COEFF_BIT_SIZE);
+
+    while (fmpq_poly_is_zero(B)) fmpq_poly_randtest(B, rand_state, length, COEFF_BIT_SIZE);
+    
+    if (fmpq_poly_euclidean_division(Q, R, A, B)) {
+      printf("ERROR: Function call returned non-zero error code.\n");
+      printf("Polynomial A:\n");
+      fmpq_poly_print_pretty(A, "x");
+      printf("\nPolynomial B:\n");
+      fmpq_poly_print_pretty(B, "x");
+      printf("\n");
+      break;
+    }
+
+    fmpq_poly_mul(S, Q, B);
+    fmpq_poly_add(S, S, R);
+    
+    if (!fmpq_poly_equal(S, A)) {
+      printf("Test %d is unsuccessful.\n");
+      printf("Polynomial A:\n");
+      fmpq_poly_print_pretty(A, "x");
+      printf("\nPolynomial B:\n");
+      fmpq_poly_print_pretty(B, "x");
+      printf("\nPolynomial Q:\n");
+      fmpq_poly_print_pretty(Q, "x");
+      printf("\nPolynomial R:\n");
+      fmpq_poly_print_pretty(R, "x");
+      printf("\nPolynomial S = Q * B + R:\n");
+      fmpq_poly_print_pretty(S, "x");
+      printf("\n");
+      continue;
+    }
+
+    printf("Test %d is successful.\n");
   }
 
-  flint_rand_clear(state);
+  flint_rand_clear(rand_state);
+  fmpq_poly_clear(Q);
+  fmpq_poly_clear(R);
+  fmpq_poly_clear(S);
+  fmpq_poly_clear(A);
+  fmpq_poly_clear(B);
 }
 
 int main() {
-
+  fmpq_poly_euclidean_division_test(12, 10);
 }
