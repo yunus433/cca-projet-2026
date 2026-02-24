3f6d4aa229fca3c0ab314b4249ef13ee7f76352c
yunus433 <yunus.gurlek@etu.sorbonne-universite.fr>
2026-02-24 14:54:40 +0100

    Refactor..

---
+++ code/src/euclidean_division/euclidean_division.c
@@ -0,0 +1,44 @@
+#include "euclidean_division.h"
+
+int fmpq_poly_euclidean_division(
+  fmpq_poly_t q,
+  fmpq_poly_t r,
+  const fmpq_poly_t a,
+  const fmpq_poly_t b
+) {
+  if (fmpq_poly_is_zero(b))
+    return -1;
+
+  fmpq_poly_zero(q);
+  fmpq_poly_set(r, a);
+
+  slong d;
+  d = fmpq_poly_degree(b);
+
+  fmpq_t c;
+  fmpq_init(c);
+  fmpq_poly_get_coeff_fmpq(c,b,d);
+
+  while (fmpq_poly_degree(r) >= d && !fmpq_poly_is_zero(r)) {
+    fmpq_poly_t s;
+    fmpq_poly_init(s);
+    slong dr = fmpq_poly_degree(r);
+    fmpq_t lc,coeff;
+    fmpq_init(lc);
+    fmpq_init(coeff);
+    fmpq_poly_get_coeff_fmpq(lc,r,dr);
+    fmpq_div(coeff,lc,c);
+    
+    fmpq_poly_set_coeff_fmpq(s, dr - d,  coeff);
+
+    fmpq_poly_add(q, q, s);
+    fmpq_poly_mul(s, s, b);
+    fmpq_poly_sub(r, r, s);
+
+    fmpq_poly_clear(s);
+    fmpq_clear(lc);
+    fmpq_clear(coeff);
+  }
+
+  return 0;
+}
