50fb45349031cfd3862a9f7e97c7a3fa3267316b
XIA Frédéric <fredericxia@hotmail.com>
2026-03-14 00:52:59 +0100

    Fix some memory issues

--- code/src/sylvester_matrix/polynomial_sylvester_matrix.c
+++ code/src/sylvester_matrix/polynomial_sylvester_matrix.c
@@ -6,14 +6,15 @@ int fmpz_poly_polynomial_sylvester_matrix(
   const fmpz_poly_t Q
 ) {
   fmpz_t tmp; // Temp values
+  fmpz_init(tmp);
 
   int n = fmpz_poly_degree(P);
   int m = fmpz_poly_degree(Q);
 
-  int N = n + m; // The Sylvester matrix has size (n+m)x(n+m)
-
-  if (m < 0 || n < 0) // If a polynomial does not exist (degree -1 in Flint)
+  if (m < 0 || n < 0) { // If a polynomial does not exist (degree -1 in Flint)
+    fmpz_clear(tmp);
     return -1;
+  }
 
   fmpz_poly_mat_zero(S);
 
@@ -22,7 +23,7 @@ int fmpz_poly_polynomial_sylvester_matrix(
       fmpz_poly_get_coeff_fmpz(tmp, P, i); // The coefficient of poly
 
       for (int j = 0; j < m; j++) { // We shift m times
-        fmpz_poly_set_coeff_si(fmpz_poly_mat_entry(S, j + (n - i), j), 0, *tmp);
+        fmpz_poly_set_coeff_fmpz(fmpz_poly_mat_entry(S, j + (n - i), j), 0, tmp);
       }
     }
     
@@ -30,7 +31,7 @@ int fmpz_poly_polynomial_sylvester_matrix(
       fmpz_poly_get_coeff_fmpz(tmp, Q, i); // The coefficient of poly
 
       for (int j = 0; j < n; j++) { // We shift n times
-        fmpz_poly_set_coeff_si(fmpz_poly_mat_entry(S, j + (m - i), j + m), 0, *tmp);
+        fmpz_poly_set_coeff_fmpz(fmpz_poly_mat_entry(S, j + (m - i), j + m), 0, tmp);
       }
     }
   } else {
@@ -38,7 +39,7 @@ int fmpz_poly_polynomial_sylvester_matrix(
       fmpz_poly_get_coeff_fmpz(tmp, Q, i); // The coefficient of poly
 
       for (int j = 0; j < n; j++) { // We shift n times
-        fmpz_poly_set_coeff_si(fmpz_poly_mat_entry(S, j + (m - i), j), 0, *tmp);
+        fmpz_poly_set_coeff_fmpz(fmpz_poly_mat_entry(S, j + (m - i), j), 0, tmp);
       }
     }
 
@@ -46,10 +47,11 @@ int fmpz_poly_polynomial_sylvester_matrix(
       fmpz_poly_get_coeff_fmpz(tmp, P, i); // The coefficient of poly
 
       for (int j = 0; j < m; j++) { // We shift m times
-        fmpz_poly_set_coeff_si(fmpz_poly_mat_entry(S, j + (n - i), j + n), 0, *tmp);
+        fmpz_poly_set_coeff_fmpz(fmpz_poly_mat_entry(S, j + (n - i), j + n), 0, tmp);
       }
     }
   }
 
+  fmpz_clear(tmp);
   return 0;
 }
