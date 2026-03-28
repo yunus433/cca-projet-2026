a62220ed379c4c10fcb0c3409913e8eb186ee600
yunus433 <yunus.gurlek@etu.sorbonne-universite.fr>
2026-03-28 20:54:59 +0100

    Add subresultant test case file and tests

---
+++ code/src/subresultant/subresultant_euclid.c
@@ -0,0 +1,128 @@
+// The implementation is only a reference and not tested, since it is on Q.
+
+#include "subresultant_euclid.h"
+
+int fmpq_poly_subresultant_euclid(
+    fmpq_t *subresultants,
+    const fmpq_poly_t P,
+    const fmpq_poly_t Q
+) {
+    if (fmpq_poly_is_zero(P) || fmpq_poly_is_zero(Q)){
+        return -1;
+    }
+    fmpq_poly_t a, b;
+    fmpq_poly_init(a);
+    fmpq_poly_init(b);
+
+    fmpq_t rho0;
+    fmpq_init(rho0);
+    slong dP=fmpq_poly_degree(P);
+    fmpq_poly_get_coeff_fmpq(rho0,P,dP);
+    fmpq_poly_make_monic(a, P);
+    fmpq_t rho1;
+    fmpq_init(rho1);
+    slong d=fmpq_poly_degree(Q);
+    fmpq_poly_get_coeff_fmpq(rho1,Q,d);
+    fmpq_poly_make_monic(b, Q);
+
+    if(fmpq_poly_degree(a)<fmpq_poly_degree(b)){
+        fmpq_poly_swap(a,b); //a >= b
+    }
+    fmpq_t *rho;
+    slong max_steps=d+1;
+
+    rho=flint_malloc(sizeof(fmpq_t)*(max_steps+5));
+
+    for(slong i=0;i<max_steps+5;i++){
+        fmpq_init(rho[i]);
+    }
+
+    fmpq_set(rho[0],rho0); 
+    fmpq_set(rho[1],rho1);
+    slong rho_len=2;
+
+    slong *n;
+    n=flint_malloc(sizeof(slong)*(max_steps+5));
+    n[0]=fmpq_poly_degree(a);
+    n[1]=fmpq_poly_degree(b);
+    slong n_len=2;
+
+    fmpq_poly_t q,r1,r2; //r2 = monic version of r1
+    fmpq_poly_init(q);
+    fmpq_poly_init(r1);
+    fmpq_poly_init(r2);
+
+
+    while(!fmpq_poly_is_zero(b)){
+        fmpq_poly_divrem(q,r1,a,b);
+        slong deg_r1=fmpq_poly_degree(r1);
+        fmpq_poly_get_coeff_fmpq(rho[rho_len],r1,deg_r1); // TODO degr1 peut etre -1 si r1 == 0
+        rho_len++;
+        fmpq_poly_make_monic(r2,r1);
+        n[n_len]=fmpq_poly_degree(r2);
+        n_len++;
+        fmpq_poly_set(a,b);
+        fmpq_poly_set(b,r2);
+    }
+
+    fmpq_t prod_rho, sigma_i, sigma_ip1, factor;
+    fmpq_init(prod_rho);
+    fmpq_init(sigma_i);
+    fmpq_init(sigma_ip1);
+    fmpq_init(factor);
+    
+    //initialisation subresultants à 0
+    for (slong k = 0; k <= d; k++) fmpq_zero(subresultants[k]);
+
+    fmpq_pow_si(subresultants[n[1]], rho[1], n[0] - n[1]);  // sigma_{n1}
+    fmpq_set(sigma_i, subresultants[n[1]]);
+
+    // prod_rho = rho0*rho1
+    fmpq_mul(prod_rho, rho[0], rho[1]); //
+
+    for (slong i = 1; i + 1 < n_len; i++) {
+        slong n_im1 = n[i - 1];
+        slong n_i   = n[i];
+        slong n_ip1 = n[i + 1];
+
+        // multiplier prod_rho par rho_{i+1}
+        fmpq_mul(prod_rho, prod_rho, rho[i + 1]);
+
+        slong exp_pow = n_i - n_ip1;  // >= 1 normalement
+
+        // factor = prod_rho^(exp_pow)
+        fmpq_pow_si(factor, prod_rho, exp_pow);
+
+        // signe
+        slong E = (n_im1 - n_i) * (n_i - n_ip1 + 1);
+        if (E & 1) fmpq_neg(factor, factor);
+
+        // sigma_{n_{i+1}} = factor * sigma_{n_i}
+        fmpq_mul(sigma_ip1, factor, sigma_i);
+
+        if (0 <= n_ip1 && n_ip1 <= d) {
+            fmpq_set(subresultants[n_ip1], sigma_ip1);
+        }
+
+        fmpq_set(sigma_i, sigma_ip1);
+    }
+
+    fmpq_clear(prod_rho);
+    fmpq_clear(sigma_i);
+    fmpq_clear(sigma_ip1);
+    fmpq_clear(factor);
+
+    fmpq_poly_clear(q);
+    fmpq_poly_clear(r1);
+    fmpq_poly_clear(r2);
+
+    for (slong i = 0; i < max_steps+5; i++) fmpq_clear(rho[i]);
+    flint_free(rho);
+    flint_free(n);
+
+    fmpq_poly_clear(a);
+    fmpq_poly_clear(b);
+    fmpq_clear(rho0);
+    fmpq_clear(rho1);
+    return 0;
+}
--- code/src/subresultant/test_subresultant.c
+++ code/src/subresultant/test_subresultant.c
@@ -1,15 +1,26 @@
-// gcc-15 -O2 ../sylvester_matrix/sylvester_matrix.c subresultant_euclid.c subresultant_naive.c subresultant_pseudo_remainder.c test_subresultant.c  $(pkg-config --cflags --libs flint)
+// gcc-15 -O2 ../sylvester_matrix/sylvester_matrix.c subresultant_naive.c subresultant_pseudo_remainder.c test_subresultant.c  $(pkg-config --cflags --libs flint)
 
 // As FLINT does not expose any functions to compute the subresultants directly,
-// this code generates random polynomials and tests if all 3 implementations of the subresultants
+// this code generates random polynomials and tests if both implementations of the subresultants
 // give the same result.
 
+// If you want to test subresultant codes against pre-generated test cases, use test_cases_subresultant.c.
+
+// Note that since subresultant_euclid is on Q, it is not tested. The implementation is only a reference.
+
 // The naive algorithm is supposed to be the "testing" algorithm.
+
+// How it works?
+// ./a.out {DEGREE} {COUNT}
+
+// DEGREE := Degree of polynomials tested.
+// COUNT := Number of tests to be performed.
+
+// Note: The coefficient bit size is set to be COEFF_BIT_SIZE as a constant, you can change it in the code.
 
 #include <time.h>
 #include <stdlib.h>
 
-#include "subresultant_euclid.h"
 #include "subresultant_naive.h"
 #include "subresultant_pseudo_remainder.h"
 
@@ -22,16 +33,15 @@ int fmpz_poly_subresultant_test(
   int count
 ) {
   flint_rand_t rand_state;
-  fmpz_t R[degree+1], S[degree+1], T[degree+1];
+  fmpz_t R[degree+1], S[degree+1];
   fmpz_poly_t A, B;
 
-  flint_randinit(rand_state);
-  flint_randseed(rand_state, time(NULL), time(NULL));
+  flint_rand_init(rand_state);
+  flint_rand_set_seed(rand_state, time(NULL), time(NULL));
 
   for (int i = 0; i <= degree; i++) {
     fmpz_init(R[i]);
     fmpz_init(S[i]);
-    fmpz_init(T[i]);
   }
 
   fmpz_poly_init(A);
@@ -45,6 +55,7 @@ int fmpz_poly_subresultant_test(
     fmpz_poly_randtest(A, rand_state, length, COEFF_BIT_SIZE);
     fmpz_poly_randtest(B, rand_state, length, COEFF_BIT_SIZE);
 
+    // TO DO: NOT USE THIS
     while (fmpz_poly_is_zero(A)) fmpz_poly_randtest(A, rand_state, length, COEFF_BIT_SIZE);
     while (fmpz_poly_is_zero(B)) fmpz_poly_randtest(B, rand_state, length, COEFF_BIT_SIZE);
     
@@ -58,17 +69,7 @@ int fmpz_poly_subresultant_test(
       break;
     }
 
-    if (fmpz_poly_subresultant_naive(S, A, B)) {
-      printf("ERROR: subresultant_euclid function call returned non-zero error code.\n");
-      printf("Polynomial A:\n");
-      fmpz_poly_print_pretty(A, "x");
-      printf("\nPolynomial B:\n");
-      fmpz_poly_print_pretty(B, "x");
-      printf("\n");
-      break;
-    }
-
-    if (fmpz_poly_subresultant_pseudo_remainder(T, A, B)) {
+    if (fmpz_poly_subresultant_pseudo_remainder(S, A, B)) {
       printf("ERROR: subresultant_naive function call returned non-zero error code.\n");
       printf("Polynomial A:\n");
       fmpz_poly_print_pretty(A, "x");
@@ -84,42 +85,42 @@ int fmpz_poly_subresultant_test(
     for (int j = 0; j < degree; j++) {
       fmpz_abs(R[j], R[j]);
       fmpz_abs(S[j], S[j]);
-      fmpz_abs(T[j], T[j]);
 
-      if (!fmpz_equal(R[j], S[j]) || !fmpz_equal(R[j], T[j])) {
+      if (!fmpz_equal(R[j], S[j])) {
         is_all_successful = 0;
 
-        printf("Test %d is unsuccessful.\n",j+1);
-        printf("Polynomial A:\n");
-        fmpz_poly_print_pretty(A, "x");
-        printf("\nPolynomial B:\n");
-        fmpz_poly_print_pretty(B, "x");
-        printf("\nR[%d] (naive):\n", j);
-        fmpz_print(R[j]);
-        printf("\nS[%d] (euclid):\n", j);
-        fmpz_print(S[j]);
-         printf("\nT[%d] (pseudo remainder):\n", j);
-        fmpz_print(T[j]);
-        printf("\n");
         break;
       }
     }
 
+    if (!is_all_successful) {
+      printf("Test %d is unsuccessful.\n", i+1);
+      printf("Polynomial A:\n");
+      fmpz_poly_print_pretty(A, "x");
+      printf("\nPolynomial B:\n");
+      fmpz_poly_print_pretty(B, "x");
+      for (int j = 0; j < degree; j++) {
+        printf("\nR[%d] (naive):\n", j);
+        fmpz_print(R[j]);
+         printf("\nS[%d] (pseudo remainder):\n", j);
+        fmpz_print(S[j]);
+        printf("\n");
+      }
+    }
+
     if (is_all_successful)
-      printf("Test %d is successful.\n",i+1);
+      printf("Test %d is successful.\n", i+1);
       for (int k = 0; k <= degree; k++) {
         fmpz_zero(R[k]);
         fmpz_zero(S[k]);
-        fmpz_zero(T[k]);
       }
   }
 
-  flint_randclear(rand_state);
+  flint_rand_clear(rand_state);
 
   for (int i = 0; i <= degree; i++) {
     fmpz_clear(R[i]);
     fmpz_clear(S[i]);
-    fmpz_clear(T[i]);
   }
 
   fmpz_poly_clear(A);
